0496608ca7807d0439a47dd1c60157a6c08b6a4b
boydndonga <boydbugle@gmail.com>
2019-02-17 01:20:00 +0300

    create password hashing

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from .. import db
 from flask_login import login_user, logout_user, login_required
 from .forms import LoginForm, RegistrationForm
 from flask import render_template, redirect, url_for
+from app.models import User
 
 
 @auth.route('/register', methods=["GET", "POST"])
