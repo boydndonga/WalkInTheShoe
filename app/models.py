6af8bdd481825a5896e1238f5739ea0386eb92ba
boydndonga <boydbugle@gmail.com>
2019-02-16 22:07:18 +0300

    create user and post model

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from . import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True, index=True, nullable=False)
+    pass_secure = db.Column(db.String(255), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+    def validate_email(self):
+        if len(self.email) != 0:
+            if re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]*\.*[com|org|edu]{3}$)", self.email):
+                return True
+            return False
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
