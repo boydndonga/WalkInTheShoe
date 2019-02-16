c87f754d489dcf02cdfaeade0ab2d5e73f7a1be2
boydndonga <boydbugle@gmail.com>
2019-02-16 22:20:13 +0300

    create setttings for environment config

--- config.py
+++ config.py
@@ -1,15 +1,27 @@
 
+
 class Config:
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    CSRF_ENABLED = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProdConfig(Config):
     pass
 
+
 class TestConfig(Config):
-    pass
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DB')
+
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DB')
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
 
+config_options = {
+    "production": ProdConfig,
+    "default": DevConfig,
+    "testing": TestConfig}
