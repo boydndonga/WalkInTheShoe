60c4c3a15e8b13f73bb529f79464ca8e8e36cc02
boydndonga <boydbugle@gmail.com>
2019-02-17 20:52:05 +0300

    include post model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique=True, index=True, nullable=False)
     pass_secure = db.Column(db.String(255), nullable=False)
-    # posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
@@ -32,9 +32,9 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.pass_secure, password)
 
 
-# class Post(db.Model):
-#     __tablename__ = 'posts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.Text)
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     author_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
